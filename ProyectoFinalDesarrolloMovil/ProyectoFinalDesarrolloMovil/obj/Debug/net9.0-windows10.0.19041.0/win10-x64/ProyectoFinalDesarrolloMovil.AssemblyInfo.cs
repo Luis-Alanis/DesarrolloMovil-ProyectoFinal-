cdb4369b5cef2ccfadd37fdfb3e5d695186bb9cf
Luis-Alanis <luis_quiroz@uadec.edu.mx>
2025-05-22 02:01:36 -0600

    Priemra versión de Creación de Clases, Views y ViewModels

--- ProyectoFinalDesarrolloMovil/ProyectoFinalDesarrolloMovil/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ProyectoFinalDesarrolloMovil.AssemblyInfo.cs
+++ ProyectoFinalDesarrolloMovil/ProyectoFinalDesarrolloMovil/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ProyectoFinalDesarrolloMovil.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinalDesarrolloMovil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0aff7041f73abd7e383855f97f615da469d9ce5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinalDesarrolloMovil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinalDesarrolloMovil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
